b3479454006d165068b5409c8433b8bf257beadf
Gbenga Omotayo <gbenga.omotayo@infor.com>
2022-06-19 14:10:17 -0500

    Added xml

--- src/test/java/com/Runner/TestRunner.java
+++ src/test/java/com/Runner/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"},plugin= {"pretty", "html:target/SwagLabsReports.html"})
+@CucumberOptions(features="src/test/resources/Features", glue= {"StepDefinitions"},plugin= {"pretty", "html:target/SwagLabsReports.html", "junit:target/SwagLabsReports.xml"})
 public class TestRunner {
 }
